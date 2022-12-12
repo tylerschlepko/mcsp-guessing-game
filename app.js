cc4740869a21bc28f17280b743a742ccd77eb908
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2022-12-12 16:29:23 -0600

    updates

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var number = 5
-var enteredValue = prompt('Guess the number')
-if(parseInt(enteredValue) === number){
-    alert('Correct!')
-} else if (parseInt(enteredValue) > number){
-    alert('Lower')
-} else {
-    alert('Higher')
+var solved = false
+var count = 1
+while (solved === false){
+    var enteredValue = prompt('Guess the number')
+    if(parseInt(enteredValue) === number){
+        alert(`Correct! It only took you ${count} guesses!`)
+        solved = true
+    } else if (parseInt(enteredValue) > number){
+        alert('Lower')
+        count += 1
+    } else {
+        alert('Higher')
+        count += 1
+    }
 }
